b0f9c87bbfb55df40188e4807f5eda00b3f2a6e6
SabinaVitola <100805214+SabinaVitola@users.noreply.github.com>
2023-11-27 16:04:20 -0500

    ModelParameters and ModelSystem are needed to run the app.
    Astrocyte_traces were provided by Nick Mennona.

---
+++ ModelParameters.m
@@ -0,0 +1,19 @@
+classdef ModelParameters
+  properties (Access = public ) %giving app access to the properties  
+        k_2 = 0.1     % uM -- Hill equation hmr for Vserca
+        k_CaA = 0.15  % uM AG --Vplc Remove until figure out oscillatory b--try overdamping with some Sines
+        k_CaI = 0.15  % uM  AG --Vplc Remove ^
+        k_deg = 0.08  % 1/s -- deg of IP3 
+        k_f = 0.5     % 1/s -- Leak flux from ER to C ADD
+        k_IP3 = 0.1   % uM -- -- Hill equation hmr2 for Vcicr
+        k_out = 0.5   % 1/s AG ADD 
+        k_p = 0.3     % uM NG Remove^-- Hill equation hmr for Vplc
+        m = 2.2
+        n = 2.02      %AG
+        v_in = 0.05   % uM/s AG -- C
+        v_M2 = 15     % uM/s AG -- Max for Vserca 
+        v_M3 = 40     % 1/s AG --Max for Vcicr
+        v_p = 0.05    % uM/s --Max for Vplc
+    end
+end
+
---
+++ ModelSystem.m
@@ -0,0 +1,37 @@
+classdef ModelSystem 
+    properties
+        params
+    end
+    
+    methods
+        function obj = ModelSystem(params)
+            if nargin > 0
+            obj.params = params;
+            end
+     end
+     function deriv = computeDeriv(obj, apl, t) %apl is the different states but MATLAB is STUPID
+            Ca = apl(1);
+            Ca_ER = apl(2);
+            IP3 = apl(3);
+            modelPar = obj.params;
+            
+            % Intermediate variables 
+            v_SERCA = modelPar.v_M2 * Ca^2 / (Ca^2 + modelPar.k_2^2);
+            v_PLC = modelPar.v_p * Ca^2 / (Ca^2 + modelPar.k_p^2);
+            v_CICR = 4 * modelPar.v_M3 * modelPar.k_CaA^modelPar.n * Ca^modelPar.n ...
+                / ( (Ca^modelPar.n + modelPar.k_CaA^modelPar.n) ...
+                * (Ca^modelPar.n + modelPar.k_CaI^modelPar.n) ) ...
+                * IP3^modelPar.m ...
+                / ( (IP3^modelPar.m + modelPar.k_IP3^modelPar.m) ) ...
+                * (Ca_ER - Ca);
+                
+            % dx/dt 
+            dCa_per_dt = modelPar.v_in - modelPar.k_out * Ca + v_CICR - v_SERCA ...
+                + modelPar.k_f * (Ca_ER - Ca);
+            dCa_ER_per_dt = v_SERCA - v_CICR - modelPar.k_f * (Ca_ER - Ca);
+            dIP3_per_dt = v_PLC - modelPar.k_deg * IP3;
+            
+            deriv = [dCa_per_dt; dCa_ER_per_dt; dIP3_per_dt]; %returns column vector
+        end
+    end
+end
